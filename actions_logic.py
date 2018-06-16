5e6a7ecf31bf93b6a405d927451be33bfc3271d1
splinterific <markgpreston@gmail.com>
2018-06-16 14:29:59 +0100

    added oauth flow and user_token to config

--- actions_logic.py
+++ actions_logic.py
@@ -1,18 +1,18 @@
 import main
 from flask import make_response
+import misc_func
 
 
 def action_calling(payload):
     """
         action endpoint, receiving payloads when user clicks the action
         grabbing the relevant values and parsing the reactions
-        """
+    """
 
     # print(json_format.pretty_json(payload))
     ts = payload["message"]["ts"]
     channel_id = payload["channel"]["id"]
-    main.thread_info(channel_id, ts)
-    # this will be swapped to use the env variable
+    misc_func.thread_info(channel_id, ts)
     if payload["token"] == main.veri:
         print("payload token ok")
         if payload["callback_id"] == "threadDis":
@@ -20,7 +20,7 @@ def action_calling(payload):
             ts = payload["message"]["ts"]
             channel_id = payload["channel"]["id"]
             user_id = payload["user"]["id"]
-            main.thread_info(channel_id, ts)
+            misc_func.thread_info(channel_id, ts)
         return make_response("OK", 200)
     else:
         return make_response("wrong token, who dis", 403)
