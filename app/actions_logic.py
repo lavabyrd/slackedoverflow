9a7549330be83cd214e337a9d73ee085f8dc0f69
splinterific <markgpreston@gmail.com>
2018-06-30 08:27:48 +0100

    updated to include an __init__ file, refactored all the installs and file names

--- app/actions_logic.py
+++ app/actions_logic.py
@@ -1,6 +1,6 @@
-import main
+import app.routes
 from flask import make_response
-import misc_func
+import app.misc_func
 
 
 def action_calling(payload):
@@ -12,15 +12,15 @@ def action_calling(payload):
     # print(json_format.pretty_json(payload))
     ts = payload["message"]["ts"]
     channel_id = payload["channel"]["id"]
-    misc_func.thread_info(channel_id, ts)
-    if payload["token"] == main.veri:
+    app.misc_func.thread_info(channel_id, ts)
+    if payload["token"] == app.routes.veri:
         print("payload token ok")
         if payload["callback_id"] == "threadDis":
             print("payload callback ok")
             ts = payload["message"]["ts"]
             channel_id = payload["channel"]["id"]
             user_id = payload["user"]["id"]
-            misc_func.thread_info(channel_id, ts)
+            app.misc_func.thread_info(channel_id, ts)
         return make_response("OK", 200)
     else:
         return make_response("wrong token, who dis", 403)
---
+++ startup.py
@@ -0,0 +1,18 @@
+from app import app, db
+# from app.models import User, Post
+import os
+
+
+# @app.shell_context_processor
+# def make_shell_context():
+#     return {'db': db, 'User': User, 'Post': Post}
+
+
+# App startup
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+
+    app.run(debug=False,
+            host="0.0.0.0",
+            port=port
+            )
