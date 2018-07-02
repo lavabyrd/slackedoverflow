8cc1f414df7515df866ead1e0c58b4492ac7fd8f
splinterific <markgpreston@gmail.com>
2018-07-02 22:32:37 +0100

    won't import app, uploading to github

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from config import Config
+
+import os
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+
 
 from app import routes
