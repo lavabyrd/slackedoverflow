b647aca01f946abefdf5c228d84438f61583580e
splinterific <markgpreston@gmail.com>
2018-07-07 09:03:50 +0100

    added a flask shell context to access the db object

--- startup.py
+++ startup.py
@@ -1,6 +1,12 @@
 from application import app, db
-
 import os
+from application.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 
 # App startup
 if __name__ == '__main__':
