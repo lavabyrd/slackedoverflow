a70745fa5720b35d94562cdd5e57874293332fc3
splinterific <markgpreston@gmail.com>
2018-07-07 09:46:27 +0100

    added login route

--- application/__init__.py
+++ application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
+from flask_login import LoginManager
 from config import Config
 
 import os
@@ -10,8 +10,8 @@ import os
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 
 from application import routes, models
