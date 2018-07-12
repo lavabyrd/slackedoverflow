a1f3b58edaf7170c80342cb8968383eadc2baaf5
splinterific <markgpreston@gmail.com>
2018-07-12 21:24:21 +0100

    removed the if __name__ == __main from the startup file

--- startup.py
+++ startup.py
@@ -11,11 +11,20 @@ def make_shell_context():
     return {'db': db, 'User': models.User, 'Post': models.Post}
 
 
-# App startup
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+# this may not be required as I'm using a top level entry point
 
-    app.run(debug=False,
-            host="0.0.0.0",
-            port=port
-            )
+# App startup
+# if __name__ == '__main__':
+#     port = int(os.environ.get('PORT', 5000))
+
+#     app.run(debug=False,
+#             host="0.0.0.0",
+#             port=port
+#             )
+
+port = int(os.environ.get('PORT', 5000))
+
+app.run(debug=False,
+        host="0.0.0.0",
+        port=port
+        )
