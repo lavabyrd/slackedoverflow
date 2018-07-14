97fd7539738c949c9ee4775797a07342aee2f3fe
splinterific <markgpreston@gmail.com>
2018-07-14 11:32:57 +0100

    fixed models and misc_func to commit to the table from the actions endpoint

--- application/misc_func.py
+++ application/misc_func.py
@@ -1,4 +1,5 @@
 from application import (
+    db,
     json_format,
     routes
 )
@@ -33,10 +34,18 @@ def post_write(team_id, team_domain, user_id, channel_id, ts):
     print("step1")
 
     for i in thread_info(channel_id, ts):
-        post = models.Post(
-            message_text=i['text'],
-            team_id=team_id,
-            team_domain=team_domain,
-            user_added=user_id
-        )
-        print(i['text'])
+        try:
+            post = models.Post(
+                message_text=i['text'],
+                team_id=team_id,
+                team_domain=team_domain,
+                user_added=user_id,
+                user_posted=i['user'],
+                ts_posted=i['ts'],
+                channel=channel_id,
+                replies=i['reply_count']
+            )
+            db.session.add(post)
+            db.session.commit()
+        except:
+            continue
