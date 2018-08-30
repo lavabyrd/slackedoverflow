267969078948aaf4f7ca9625804fb2e596e05dba
splinterific <markgpreston@gmail.com>
2018-08-30 17:22:31 +0100

    updated to remove dodgy lines in the manage.py file

--- manage.py
+++ manage.py
@@ -21,9 +21,5 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-<< << << < HEAD
-== == == =
-
->>>>>> > testing
 if __name__ == '__main__':
     manager.run()
